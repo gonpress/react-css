e88d2aef82b08c4187ffb44260e65ecc26754b0e
gonpress <gonpress@naver.com>
2022-02-13 12:31:14 +0900

    tv api 적용

--- src/screens/Tv.js
+++ src/screens/Tv.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {tvsApi} from "../Api";
+
 
 const Tv = () => {
+    const [tvs, setTvs] = useState({
+        onTheAir: [],
+        popular: [],
+        latest: [],
+    })
+
+    const getTvs = async () => {
+        try{
+            const onTheAir = await tvsApi.onTheAir();
+            const popular = await tvsApi.popular();
+            const latest = await tvsApi.latest();
+
+            setTvs({
+                onTheAir:onTheAir.data.results,
+                popular:popular.data.results,
+                latest:latest.data.results,
+            })
+        }catch (e) {
+            console.log(e);
+        }
+    }
+
+    useEffect(() => {
+        getTvs();
+    }, []);
+
+
     return (
         <div>
             <h1>Tv</h1>
+            {tvs.onTheAir.length}
         </div>
     );
 };
